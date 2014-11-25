9fa5ed2ae89047ccfb1664be5e71f3766d264bfc
Derrick Johnson <derrickjohnson@eharmony.com>
2014-11-25 09:16:03 -0800

    updated env_file

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module GitJiraIntegration
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-
+    env_file = File.join(Rails.root, 'config', 'local_env.yml')
     YAML.load(File.open(env_file)).each do |key, value|
         ENV[key.to_s] = value
     end if File.exists?(env_file)
