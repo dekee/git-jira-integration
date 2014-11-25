2e8f4c6ca9611801c85f8a9ffac1ba45111f543f
Derrick Johnson <derrickjohnson@eharmony.com>
2014-11-25 08:43:34 -0800

    updated to take advantage of environment variables

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,9 @@ module GitJiraIntegration
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+    end if File.exists?(env_file)
   end
 end
